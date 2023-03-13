1455c8c4e3bfcbc43be3e11253e385a394897cf8
TC <tudor.campean23@gmail.com>
2023-03-13 20:36:00 +0200

    Add files via upload

---
+++ ray-tracer-tcampean/RayTracer.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace rt
+{
+    class RayTracer
+    {
+        private Geometry[] geometries;
+        private Light[] lights;
+
+        public RayTracer(Geometry[] geometries, Light[] lights)
+        {
+            this.geometries = geometries;
+            this.lights = lights;
+        }
+
+        private double ImageToViewPlane(int n, int imgSize, double viewPlaneSize)
+        {
+            var u = n * viewPlaneSize / imgSize;
+            u -= viewPlaneSize / 2;
+            return u;
+        }
+
+        private Intersection FindFirstIntersection(Line ray, double minDist, double maxDist)
+        {
+            var intersection = new Intersection();
+
+            foreach (var geometry in geometries)
+            {
+                var intr = geometry.GetIntersection(ray, minDist, maxDist);
+
+                if (!intr.Valid || !intr.Visible) continue;
+
+                if (!intersection.Valid || !intersection.Visible)
+                {
+                    intersection = intr;
+                }
+                else if (intr.T < intersection.T)
+                {
+                    intersection = intr;
+                }
+            }
+
+            return intersection;
+        }
+
+        private bool IsLit(Vector point, Light light)
+        {
+            // ADD CODE HERE: Detect whether the given point has a clear line of sight to the given light
+            Line ray = new Line(light.Position, point);
+            var intersection = FindFirstIntersection(ray, 0.0, (light.Position - point).Length());
+
+            return !intersection.Valid;
+        }
+
+        public void Render(Camera camera, int width, int height, string filename)
+        {
+            var background = new Color();
+            var image = new Image(width, height);
+
+            for (var i = 0; i < width; i++)
+            {
+                for (var j = 0; j < height; j++)
+                {
+                    // ADD CODE HERE: Implement pixel color calculation
+                    var viewParallel = camera.Up ^ camera.Direction;
+                    var vectorX1 = camera.Position +
+                       camera.Direction * camera.ViewPlaneDistance +
+                       camera.Up * ImageToViewPlane(j, height, camera.ViewPlaneHeight) +
+                       viewParallel * ImageToViewPlane(i, width, camera.ViewPlaneWidth);
+                    var intersection = FindFirstIntersection(new Line(camera.Position, vectorX1), camera.FrontPlaneDistance, camera.BackPlaneDistance);
+                    if (intersection.Geometry == null) {
+                        image.SetPixel(i, j, new Color());
+                    }
+                    else {
+                        Color objectColor = new Color();
+                        Material objectMaterial = intersection.Geometry.Material;
+                        foreach (var light in lights) {
+                            var lightColor = objectMaterial.Ambient * light.Ambient;
+
+                            if (IsLit(intersection.Position, light)) {
+                                var vectorN = (intersection.Position - ((Sphere)intersection.Geometry).Center).Normalize();
+                                var vectorT = (light.Position - intersection.Position).Normalize();
+
+                                if (vectorN * vectorT > 0) {
+                                    lightColor += objectMaterial.Diffuse * light.Diffuse * (vectorN * vectorT);
+                                }
+                                var vectorE = (camera.Position - intersection.Position).Normalize();
+                                var vectorR = vectorN * (vectorN * vectorT) * 2 - vectorT;
+
+                                if (vectorE * vectorR > 0) {
+                                    lightColor += objectMaterial.Specular * light.Specular * Math.Pow((vectorE * vectorR), objectMaterial.Shininess);
+                                }
+
+                                lightColor *= light.Intensity;
+                            }
+                            objectColor += lightColor;
+                        }
+                        image.SetPixel(i, j, objectColor);
+                    }
+                }
+            }
+
+            image.Store(filename);
+        }
+    }
+}
---
+++ ray-tracer-tcampean/Sphere.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace rt
+{
+    public class Sphere : Geometry
+    {
+        public Vector Center { get; set; }
+        public double Radius { get; set; }
+
+        public Sphere(Vector center, double radius, Material material, Color color) : base(material, color)
+        {
+            Center = center;
+            Radius = radius;
+        }
+        
+        public override Intersection GetIntersection(Line line, double minDist, double maxDist)
+        {
+            // ADD CODE HERE: Calculate the intersection between the given line and this sphere
+            var posClosestCenter = (Center - line.X0) * line.Dx;
+            var vectorToClosest = line.CoordinateToPosition(posClosestCenter);
+            var distanceCenterToVector = (Center - vectorToClosest).Length();
+            var distanceBetweenPoints = Math.Sqrt(Radius * Radius - distanceCenterToVector * distanceCenterToVector);
+            if (posClosestCenter > minDist && posClosestCenter < maxDist && distanceCenterToVector <= Radius) {
+                return new Intersection(true, true, this, line, posClosestCenter - distanceBetweenPoints);
+            }
+            return new Intersection();
+        }
+        public override Vector Normal(Vector v)
+        {
+            var n = v - Center;
+            n.Normalize();
+            return n;
+        }
+    }
+}
